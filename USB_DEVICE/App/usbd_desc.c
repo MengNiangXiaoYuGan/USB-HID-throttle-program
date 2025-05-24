f26bb36c6ebd33dadd00bf15e6e9a86fb8829ff5
MengNiangXiaoYuGan <824616398@qq.com>
2025-05-24 08:28:36 +0800

    脚舵简易版编写完成，兼容油门电路板，未修改HID报文

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -62,11 +62,11 @@
   * @{
   */
 
-#define USBD_VID     1154
+#define USBD_VID     1153
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22353
-#define USBD_PRODUCT_STRING_FS     "HID_throttle"
+#define USBD_PID_FS     22352
+#define USBD_PRODUCT_STRING_FS     "HID_rudders"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
 #define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
 
